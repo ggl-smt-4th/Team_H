09085601ed3f922acca8b6f28ff43bdded40a864
q734550709 <39423749+q734550709@users.noreply.github.com>
2018-06-20 07:42:57 +0800

    Update yours.sol

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,55 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+//设计思路：定义雇员地址、增加资金池、查看工资池剩余、付出工资、查看员工工资余额
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+    }
+
+    function updateEmployee(address e, uint s) {
+        require(msg.sender == owner);
+
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
+    }
+
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+
+    function getPaid() {
+        require(msg.sender == employee);
+
+        uint nextPayday = lastPayday + payDuration;
+        if (nextPayday > now){
+            revert();
+        }
+        else
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+    function employeefund() payable returns (uint) {
+        return this.balance;
+    }
+}
