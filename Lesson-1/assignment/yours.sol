fd8e8bfc1c390d73599c5f1c98ee73200ede1c50
paul89114 <38344846+paul89114@users.noreply.github.com>
2018-06-20 21:07:41 +0800

    Update yours.sol

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,46 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint salary = 0;
+    address employee;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    function setSalary (uint x) {
+        salary = x * 1 ether;
+    }
+    
+    function setAddress (address add) {
+        employee = add;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        if (salary == 0) {
+            revert();
+        }
+        return this.balance/salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        if (msg.sender != employee || employee == 0 || salary == 0) {
+            revert();
+        }
+
+        uint nextPayday = lastPayday + payDuration;
+
+        if (nextPayday > now) {
+            revert();
+        }
+        
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
